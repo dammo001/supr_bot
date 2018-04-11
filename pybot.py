387753dcb41b4ee0f349919f2970cac7cc7d671c
Jonathan Tung <jtung23@gmail.com>
2018-04-10 22:26:02 -0700

    first commit

---
+++ pybot.py
@@ -0,0 +1,30 @@
+from lxml import html
+import requests
+from lxml import etree
+
+def text_tail(node):
+    yield node.text
+    yield node.tail
+
+url='https://www.supremenewyork.com/shop/all'
+
+response = requests.get(url) #get page data from server, block redirects
+sourceCode = response.content #get string of source code from response
+htmlElem = html.document_fromstring(sourceCode) #make HTML element object
+
+listClass =  htmlElem.find_class('inner-article')
+for elem in listClass:
+	# stringelem = html.tostring(elem)
+	print(html.tostring(elem.xpath('//a')[0]))
+	# print(elem.xpath('//a')[0].get('href'))
+
+
+
+# doc=lh.parse(urllib.request.urlopen(url).read())
+# tree = etree.fromstring(doc)
+# for elem in tree.xpath('/html/body/div/div//article'):
+# 	print(etree.tostring(elem, pretty_print=True))
+
+
+
+
